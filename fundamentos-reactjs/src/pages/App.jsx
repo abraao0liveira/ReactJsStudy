b4e8ecdf6ac25e1d15a0b6b124d9c9eeabe1e7ce
Abraão Oliveira <113057132+abraao0liveira@users.noreply.github.com>
2023-03-30 12:01:28 -0300

    add component Sidebar.jsx and Sidebar.module.css

--- fundamentos-reactjs/src/pages/App.jsx
+++ fundamentos-reactjs/src/pages/App.jsx
@@ -1,6 +1,8 @@
 import { Post } from './Post'
 import '../styles/global.css'
 import { Header } from '../components/Header/Header'
+import styles from './App.module.css'
+import { Sidebar } from '../components/Sidebar/Sidebar'
 
 export function App() {
 
@@ -8,14 +10,20 @@ export function App() {
     <div>
       <Header />
 
-      <Post 
-        author="Abraão Oliveira" 
-        content="Olá, meu nome é Abraão Oliveira e eu sou um desenvolvedor front-end." 
-      />
-      <Post 
-        author="Gabriel Oliveira" 
-        content="Olá, meu nome é Gabriel Oliveira e eu sou um desenvolvedor back-end." 
-      />
+      <div className={styles.wrapper}>
+        <Sidebar />
+
+        <main>
+          <Post 
+          author="Abraão Oliveira" 
+          content="Olá, meu nome é Abraão Oliveira e eu sou um desenvolvedor front-end." 
+          />
+          <Post 
+          author="Gabriel Oliveira" 
+          content="Olá, meu nome é Gabriel Oliveira e eu sou um desenvolvedor back-end." 
+          />
+        </main>
+      </div>
     </div>
   )
 }
